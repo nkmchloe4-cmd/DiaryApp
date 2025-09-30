a3cf4deae79a188a6ed37ef2fb1454c89540670b
Chloe Nakamanya <nakamanyachloe4@gmail.com>
2025-09-30 16:04:14 +0200

    Add project files.

---
+++ MenuChoices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryApp
+{
+    public enum MenuChoices
+    {
+        AddEntry = 1,
+        ShowAllEntries = 2,
+        SearchEntries = 3,
+        SaveToFile = 4,
+        ReadFromFile = 5,
+        Exit = 6
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiaryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
