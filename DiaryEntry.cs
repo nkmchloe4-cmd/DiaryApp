6aa2f4119364ddebce32a9afa93422182f5e3cc2
Chloe Nakamanya <nakamanyachloe4@gmail.com>
2025-09-30 18:02:56 +0200

    Added Menu and DiaryEntry Class

---
+++ DiaryEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiaryApp
+{
+    public class DiaryEntry
+    {
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+
+public DiaryEntry(DateTime date, string text)
+        {
+            Date = date;
+            Text = text;
+        }
+
+    }
+     
+ }
+
--- Program.cs
+++ Program.cs
@@ -1,9 +1,42 @@
-﻿namespace DiaryApp
+﻿using System.IO;
+
+namespace DiaryApp
 {
     internal class Program
     {
+        private static readonly string diaryFilePath = "diary.txt";
         static void Main(string[] args)
         {
+            Console.WriteLine("Välkommen Till DagboksAppen!");
+
+            while (true)
+            {
+                Console.WriteLine("\nVad vill du göra?");
+                Console.WriteLine("1. Skriv ny anteckning");
+                Console.WriteLine("2. Visa alla anteckningar ");
+                Console.WriteLine("3. Sök anteckning på datum");
+                Console.WriteLine("4. Spara till fil");
+                Console.WriteLine("5. Läs från fil");
+                Console.WriteLine("6. Avsluta Appen");
+                Console.WriteLine("Skriv in din val");
+
+                MenuChoices choice = GetMenuChoices();
+
+
+            }
+
+        }
+        private static MenuChoices GetMenuChoices()
+        {
+            string? input = Console.ReadLine();
+            if (Enum.TryParse<MenuChoices>(input, true, out MenuChoices choice) 
+                && Enum.IsDefined(typeof(MenuChoices), choice))
+            {
+                return choice;
+            }
+            Console.WriteLine("Ogiltigt val. Försök igen.");
+            return GetMenuChoices();
+
         }
     }
 }
